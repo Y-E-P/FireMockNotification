62f508efa334abbc541f65038524c53da2b07135
Yuriy Poudanen <mcpchip@gmail.com>
2022-01-12 01:02:57 +0200

    * moved menu

---
+++ src/main/kotlin/menu.kt
@@ -0,0 +1,52 @@
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.input.key.Key
+import androidx.compose.ui.input.key.KeyShortcut
+import androidx.compose.ui.window.FrameWindowScope
+import androidx.compose.ui.window.MenuBar
+import resources.ResString
+
+@OptIn(ExperimentalComposeUiApi::class)
+@Composable
+fun FrameWindowScope.FireMenu(onClickItem: (MenuItem) -> Unit) {
+    MenuBar {
+        Menu(ResString.file, mnemonic = 'F') {
+            Item(
+                ResString.saveScheme,
+                enabled = true,
+                onClick = { onClickItem(MenuItem.SAVE) },
+                shortcut = KeyShortcut(Key.C, ctrl = true)
+            )
+            Item(
+                ResString.saveAsScheme,
+                enabled = true,
+                onClick = { onClickItem(MenuItem.SAVE_AS) },
+                shortcut = KeyShortcut(Key.V, ctrl = true)
+            )
+            Item(
+                ResString.loadScheme,
+                enabled = true,
+                onClick = { onClickItem(MenuItem.OPEN) },
+                shortcut = KeyShortcut(Key.V, ctrl = true)
+            )
+        }
+        Menu(ResString.help, mnemonic = 'A') {
+            Item(
+                ResString.faq,
+                enabled = false,
+                onClick = { onClickItem(MenuItem.ABOUT) },
+                shortcut = KeyShortcut(Key.V, ctrl = true)
+            )
+            Item(
+                ResString.about,
+                enabled = true,
+                onClick = { onClickItem(MenuItem.ABOUT) },
+                shortcut = KeyShortcut(Key.V, ctrl = true)
+            )
+        }
+    }
+}
+
+enum class MenuItem {
+    SAVE, SAVE_AS, OPEN, ABOUT
+}
